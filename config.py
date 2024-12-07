4610aae577574d9a8c8a7534d8690efe64ce5c2b
Alex <154541168+BruhWhyMeHaveNoIdea@users.noreply.github.com>
2024-12-07 14:34:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
-BOT_TOKEN = "7335689267:AAG0CN0dvjYOTIG-5PZrWPtD539kYEuS64o"
-GPT_TOKEN = ("sk-proj-a-DglDA46AgozkXmUqt5z-E_YltPMF5VnpNVZ33LlQlgpCZt--sZ8579uZOFA0FO7wPwCrgd8bT3BlbkFJ2sv"
-             "LapcabkaOda0Bg1GCoqmzqbi_jHvA6lCeNNK330Q5Xtvm2Mc3aornzVyTRM00HjOVM37S8A")
+BOT_TOKEN = ...
+GPT_TOKEN = ...
 
 
-provider_token = "390540012:LIVE:61613"
-user = "admin"
-password = "4fda93d162ca"
+provider_token = ...
+user = ...
+password = ...
 host = "localhost"
 port = 5432
-database = "my_db"
+database = ...
 DB_CONNECTION_URL = f"postgresql+asyncpg://{user}:{password}@{host}/{database}"
 
